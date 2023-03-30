355af46be1fad6450ea01b726ae0125149624cc4
RenzoDavila <rjacosta@stefanini.com>
2023-03-30 15:28:42 -0500

    agregando ngx-toaster

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
 import { LoginComponent } from './components/login/login.component';
 import { PublicModule } from './modules/public/public.module';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,6 +19,9 @@ import { PublicModule } from './modules/public/public.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ToastrModule.forRoot(),
     PublicModule
   ],
   providers: [],
--- src/app/modules/public/components/home/home.component.ts
+++ src/app/modules/public/components/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { BlogService } from '../../services/blog/blog.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
+  blogs:any
 
+  constructor(
+    private _blogService: BlogService,
+    private toastr: ToastrService
+  ){ }
+
+  ngOnInit(): void {
+    this.getBlogs();
+  }
+
+  getBlogs() {
+    this.toastr.info('cargando...', 'obteniendo los blogs')
+    this._blogService.getBlogs().subscribe(
+      (data) => {
+        console.log("data", data)
+        this.blogs = data
+        this.toastr.info('Excito!!!', 'blogs cargados correctamente')
+
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
 }
